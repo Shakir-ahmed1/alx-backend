a54d0a496705d2fc10703010ded680d569e764b0
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-01-02 07:50:33 +0000

    revert default parameters

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -39,7 +39,7 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: Union[int, None],
+    def get_hyper_index(self, index: int = None,
                         page_size: int = 10) -> Dict:
         """ getting deletion resisting page
             """
