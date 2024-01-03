e25c48d19fc99ec861491ec50c33d1a72ee68f20
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-01-03 13:37:59 +0000

    fixing for correct output

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -8,9 +8,8 @@ class MRUCache(BaseCaching):
     def __init__(self):
         """ initialization """
         super().__init__()
-        self.keys_holder = []
-        self.usage = []
-
+        self.stack = []
+    
     def put(self, key, item):
         """ add an item to the cache """
         if key is None or item is None:
@@ -18,27 +17,22 @@ class MRUCache(BaseCaching):
         else:
             size = len(self.cache_data)
             if size + 1 > BaseCaching.MAX_ITEMS and key not in self.cache_data:
-                evaluate = list(map(lambda x: x + self.usage[x], range(size)))
-                reversed_evaluate = evaluate[::-1]
-                reversed_index = reversed_evaluate.index(max(evaluate))
-                pop_idx = len(evaluate) - reversed_index - 1
-                popped = self.keys_holder.pop(pop_idx)
-                self.usage.pop(pop_idx)
+                
+                popped = self.stack.pop()
+                while popped not in self.cache_data:
+                    popped = self.stack.pop()
                 del self.cache_data[popped]
                 print(f"DISCARD: {popped}")
-            if key in self.keys_holder:
-                rm_idx = self.keys_holder.index(key)
-                self.keys_holder.pop(rm_idx)
-                self.usage.pop(rm_idx)
-            self.keys_holder.append(key)
-            self.usage.append(0)
+
+            self.stack.append(key)
             self.cache_data[key] = item
 
     def get(self, key):
-        """ get an item from the cache """
+        """ gets an item from the cache """
         result = self.cache_data.get(key, None)
         if result is None:
             return None
         else:
-            self.usage[self.keys_holder.index(key)] += self.MAX_ITEMS - 1
+            self.stack.append(key)
             return result
+
