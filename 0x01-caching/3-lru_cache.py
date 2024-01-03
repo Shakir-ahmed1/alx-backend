19a9735c8c9ff9939eeb8c731fd8981c79fc23a2
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-01-03 13:42:22 +0000

    fixing for correct output

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -8,8 +8,8 @@ class LRUCache(BaseCaching):
     def __init__(self):
         """ initialization """
         super().__init__()
-        self.stack = []
-    
+        self.queue = []
+
     def put(self, key, item):
         """ add an item to the cache """
         if key is None or item is None:
@@ -17,22 +17,23 @@ class LRUCache(BaseCaching):
         else:
             size = len(self.cache_data)
             if size + 1 > BaseCaching.MAX_ITEMS and key not in self.cache_data:
-                
-                popped = self.stack.pop()
+                popped = self.queue.pop(0)
                 while popped not in self.cache_data:
-                    popped = self.stack.pop()
+                    popped = self.queue.pop(0)
                 del self.cache_data[popped]
                 print(f"DISCARD: {popped}")
-
-            self.stack.append(key)
+            if key in self.queue:
+                self.queue.remove(key)
+            self.queue.append(key)
             self.cache_data[key] = item
 
     def get(self, key):
-        """ gets an item from the cache """
+        """ get an item from the cache """
         result = self.cache_data.get(key, None)
         if result is None:
             return None
         else:
-            self.stack.append(key)
+            if key in self.queue:
+                self.queue.remove(key)
+            self.queue.append(key)
             return result
-
