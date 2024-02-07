5e92854390b47abeed278b3272f73d7a2f0ebaa2
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-02-07 06:26:38 +0300

    upload codebase

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,9 @@
+import { createClient } from 'redis';
+
+const client = createClient()
+  client.on('error', err => console.log(`Redis client not connected to the server: ${err}`))
+  client.on('data', () => console.log('Redis client connected to the server'))
+
+// await client.set('key', 'value');
+// const value = await client.get('key');
+// await client.disconnect();
