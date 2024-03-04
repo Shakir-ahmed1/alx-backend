83a11b676289fa3802b90d20cb8fbe612d3623c0
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-03-04 22:12:26 +0300

    submit code

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -5,18 +5,22 @@ from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
 
+
 class Config:
     """ bable configuration """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app.config.from_object(Config)
+
 
 @app.route('/')
 def home_page():
     """ home page"""
     return render_template('1-index.html')
+
 
 if __name__ == "__main__":
     app.run()
