d6a828596f626003146e8914e0fdd851ceb12552
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-03-06 16:13:52 +0300

    task 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -28,7 +28,7 @@ def get_locale():
 @app.route('/')
 def home_page():
     """ home page"""
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == "__main__":
